a6610b5e906084ad9eebe73f4d190f79ffd05d39
ruspedpdx <46876836+ruspedpdx@users.noreply.github.com>
2024-10-21 21:20:08 -0700

    added color to result text (#1)
    * added color to result text
    
    * added recommended changes
    
    ---------
    
    Co-authored-by: Russ Pedersen <ruspedamazon@gmail.com>

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,28 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const log = document.getElementById("result");
 
-elem.addEventListener('input', handleInput);
+const handleInput = () => {
+  const value = elem.value;
+
+  // Convert input to a number
+  const numberValue = Number(value);
+
+  // Check if the value is positive
+  if (numberValue < 0) {
+    log.textContent = "Please enter a positive number.";
+    log.style.color = "#8B0000";
+  }
+
+  // Check if the input is a palindrome
+  else if (value.length > 0) {
+    if (value === value.split("").reverse().join("")) {
+      log.textContent = "Yes. This is a palindrome!";
+      log.style.color = "#06402B";
+    } else {
+      log.textContent = "No. Try again.";
+      log.style.color = "#8B0000";
+    }
+  }
+};
+
+elem.addEventListener("input", handleInput);
